e29f5072ca121c0ff0207928c8df75df9bc01544
tourlivebr <tourlivebr@gmail.com>
2025-05-21 11:26:14 -0300

    atualiza001

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     // Redireciona ao acessar a home
-    window.location.href = "/cartao/flp"; // Muda para o slug desejado
+    window.location.href = "/cartao/index-sc"; // Muda para o slug desejado
   }, []);
 
   return <p>Carregando...</p>;
